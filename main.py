5f80a216531cf720b3165561637b3bf32107697f
charlesfjacques <tacochat7@gmail.com>
2025-04-04 14:29:40 -0400

    test update

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import webbrowser
 import subprocess
 import voice
 
+
 name = input("Enter player name: ")
 print(name)
 
@@ -53,7 +54,7 @@ r = requests.get(link)
 soup = BeautifulSoup(r.text, 'html.parser')
 
 try:
-        i = 0
+        i = 1
         webpage_text = soup.find_all('p')[i].get_text()
 
         if webpage_text[0] != checker:
@@ -66,46 +67,3 @@ except:
 
 voice(speech,34)
 
-
-
-
-
-
-
-
-def process_weather_request() :
-    print('processing weather request')
-
-def process_sports_request() :
-    print('processing sports request')
-
-def process_music_request() :
-    print('processing music request')
-
-def process_current_event_request() :
-    print('processing current event request')
-#
-#  This is the main driver program for the "Alexa" Project
-#
-categories = ['WEATHER','SPORTS','MUSIC','CURRENT EVENTS','EXIT']
-user_category = ''
-print("Welcome to Alexa")
-while user_category != 'EXIT' :
-    voice('Please select one of the following categories for your question',34)
-    voice('\n''WEATHER\nSPORTS\nMUSIC\nCURRENT EVENTS\nEXIT\n',34)
-    user_category = input('What is your choice : ').upper()
-    #print(user_category)
-
-    if user_category in categories :
-        if user_category == 'WEATHER' :
-            process_weather_request()
-        elif user_category == 'SPORTS' :
-            process_sports_request()
-        elif user_category == 'MUSIC' :
-            process_music_request()
-        elif user_category == 'CURRENT EVENTS' :
-            process_current_event_request()
-        else :
-            exit(0)
-    else :
-        voice('Sorry, your response does not match any of the categories',34)
