fa1f16a310019d94cfdb8aa9fbd63cf0ddc3a446
Charles-Roush <charliesurfs0@gmail.com>
2025-04-11 18:07:28 -0400

    added ignore and sports

--- sports/sports.py
+++ sports/sports.py
@@ -4,6 +4,122 @@ from pyttsx3_voice import voice
 import webbrowser
 import subprocess
 
+def player(input):
+ 
+
+      if ' ' in input:
+ 
+
+           first_last = input.replace(' ','_')
+ 
+
+      else:
+ 
+
+           first_last = input
+ 
+
+
+ 
+
+      letter_list = []
+ 
+
+      for letters in input:
+ 
+
+           letter_list.append(letters)
+ 
+
+
+ 
+
+      checker = letter_list[0]
+ 
+
+      print(checker)
+ 
+
+
+ 
+
+      print(f"Formatted name for URL: {first_last}")
+ 
+
+
+ 
+
+
+ 
+
+      search_term = f'{input}'
+ 
+
+      url = "https://www.google.com.tr//search?q={}".format(search_term)
+ 
+
+      webbrowser.open(url)
+ 
+
+
+ 
+
+      link = "https://en.wikipedia.org/wiki/{}".format(first_last)
+ 
+
+      print(f"Fetching URL: {link}")
+ 
+
+
+ 
+
+      r = requests.get(link)
+ 
+
+      soup = BeautifulSoup(r.text, 'html.parser')
+ 
+
+
+ 
+
+      try:
+ 
+
+            i = 1
+ 
+
+            webpage_text = soup.find_all('p')[i].get_text()
+ 
+
+
+ 
+
+            if webpage_text[0] != checker:
+ 
+
+                webpage_text = soup.find_all('p')[i+1].get_text()
+ 
+
+
+ 
+
+            print(webpage_text[0:1000])
+ 
+
+            speech = webpage_text[0:100]
+ 
+
+      except:
+ 
+
+            speech = "Player not found"
+ 
+
+
+ 
+
+      voice(speech,34)
+
 def process_sports_request() :
     print('processing sports request')
     sports = ['BASKETBALL','FOOTBALL','SOCCER','BASEBALL']
