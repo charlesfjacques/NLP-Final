7657f088cc0859bcee46981fefb38268307d8c1f
charlesfjacques <tacochat7@gmail.com>
2025-04-14 14:52:03 -0400

    more work

--- sports/sports.py
+++ sports/sports.py
@@ -5,15 +5,22 @@ import webbrowser
 import subprocess
 
 
-def player(input):
-      if ' ' in input:
-           first_last = input.replace(' ','_')
+def check_exit(string):
+    if 'EXIT' in string.upper():
+        voice('Returning to home', 34)
+        return True
+    return False
+
+
+def player(name):
+      if ' ' in name:
+           first_last = name.replace(' ','_')
       else:
-           first_last = input
+           first_last = name
 
       letter_list = []
  
-      for letters in input:
+      for letters in name:
            letter_list.append(letters)
  
       checker = letter_list[0]
@@ -52,22 +59,48 @@ def player(input):
  
       voice(speech,34)
 
+def sport_spliter(sport_name):
+
+      sports = ['BASKETBALL','FOOTBALL','SOCCER','BASEBALL','EXIT']
+      voice('Select a sport, state an athletes name, or say exit to return to home',34)
+
+      if sport_name in sports:
+            if sport_name == 'BASKETBALL':
+                 print('basketball')
+            elif sport_name == 'FOOTBALL':
+                 print('football')
+            elif sport_name == 'SOCCER':
+                 print('soccer')
+            elif sport_name == 'BASEBALL':
+                 print('baseball')
+            elif sport_name == 'EXIT':
+                 voice('Returning to home',34)
+      else:
+            voice('Sorry, unable to proccess that sport. Try another sport or say exit to return to home',34)
+            exit
+
+
 def process_sports_request() :
-    print('processing sports request')
-    sports = ['BASKETBALL','FOOTBALL','SOCCER','BASEBALL']
-    sport_catagory = ''
-    while sport_catagory != 'EXIT':
-        voice('Select a sport or say exit to return to home',34)
-        sport_catagory = input('What is your choice: ').upper()
-        if sport_catagory in sports:
-              if sport_catagory == 'BASKETBALL':
-                    print('basketball')
-              elif sport_catagory == 'FOOTBALL':
-                    print('football')
-              elif sport_catagory == 'SOCCER':
-                    print('soccer')
-              elif sport_catagory == 'BASEBALL':
-                    print('baseball')
-        else:
-              voice('Sorry, unable to proccess that sport. Try another sport or say exit to return to home',34)
+      print('processing sports request')
+
+      
+      while True:
+            voice('If you would like to learn about an atlhete say athlete, if you would like to learn about a sport say sport, if you would like to reutrn home say exit',34)
+            answer = input('Choice:' ).upper
+
+            if check_exit(answer):
+                 return
+            
+            if answer == 'ATHLETE':
+                 voice("state the name of the athlete",34)
+                 name = input('athlete name: ')
+                 player(name)
+            elif answer == 'SPORT':
+                 voice('state the sport you would like to investigate',34)
+                 sport_name = input('state sport: ').upper()
+                 sport_spliter(sport_name)
+            else:
+                 voice('sorry could not process your request',34)
+                 exit
+
 
